c73392554351cd075c94d2bdf4ee642fc99d5413
Shunsuke Ono <ono0112sf@gmail.com>
2022-02-22 09:13:32 +0900

    #8 ステータスと優先度の編集実装

--- pages/components/organisms/layout/Status.jsx
+++ pages/components/organisms/layout/Status.jsx
@@ -7,18 +7,31 @@ import { todosState } from "../../../atoms/atom";
 const Status = () => {
   const { query } = useRouter()
   const [todos, setTodos] = useRecoilState(todosState);
-    
   const editTodo = todos.filter((todo) => {
     return todo.id === Number(query.id);
   })
 
-  const handleChange = (e) => {
-    setTodos(todos.map((todo) => {
-      if (todo.id === editTodo[0]?.id) {
-        return todo.title = e.target.value;
+  const hadleEditStatus = (status) => {
+    const foundTodo = todos.findIndex((todo) => todo.id === editTodo[0]?.id);
+
+    const replaceItemAtIndex = (todos, foundTodo, newValue) => {
+      return [
+        ...todos.slice(0, foundTodo),
+        newValue,
+        ...todos.slice(foundTodo + 1),
+      ];
+    };
+    setTodos(() => {
+      if (todos[foundTodo].status) {
+        return replaceItemAtIndex(todos, foundTodo, {
+          ...todos[foundTodo],
+          status: status,
+        });
       }
-    }))
-  }
+    });
+  };
+
+ 
 
   return (
     <>
@@ -37,7 +50,7 @@ const Status = () => {
             borderColor="#bebaba"
             borderWidth="2px"
             value={editTodo[0]?.status}
-            onChange={handleChange}
+            onChange={(e)=>hadleEditStatus(e.target.value)}
           >
             <option value="着手前">着手前</option>
             <option value="進行中">進行中</option>
