9ee7f301e94437be9296f467daff380b94ff038a
Shunsuke Ono <ono0112sf@gmail.com>
2022-02-23 15:19:55 +0900

    #10 不要なコードの削除

--- pages/components/TodoList.js
+++ pages/components/TodoList.js
@@ -54,7 +54,7 @@ const TodoList = () => {
     }
   };
 
-  console.log(todos);
+  
 
   return (
     <>
--- pages/components/atoms/PrioritySelect.jsx
+++ pages/components/atoms/PrioritySelect.jsx
@@ -23,7 +23,7 @@ const PrioritySelect = ({ todo }) => {
         });
       }
     });
-    console.log();
+   
   };
   return (
     <select
