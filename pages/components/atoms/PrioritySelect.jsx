c0cf3a0f1b3b4c07a9278403153c683649cbbd2c
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2022-03-27 08:32:24 +0900

    #48 indexページのレスポンシブ対応

--- pages/components/atoms/PrioritySelect.jsx
+++ pages/components/atoms/PrioritySelect.jsx
@@ -22,13 +22,13 @@ const PrioritySelect = ({ todo }) => {
         });
       }
     });
-   
+    
   };
   return (
     <select
       value={todo.priority}
       onChange={(e) => handleSetNewPriority(todo.id, e.target.value)}
-      style={{ outline: "none" }}
+      style={{fontSize:"14px"}}
     >
       <option value="高">高</option>
       <option value="中">中</option>
